047b4928ecc4651b8820eecccbce08222e11afd0
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-26 21:54:15 -0400

    Add `Rubyface::BackColor`

---
+++ lib/rubyface/back_color.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Rubyface
+  # Color for the back of the card.
+  class BackColor < T::Enum
+    enums do
+      BLUE = new
+      RED = new
+    end
+  end
+end
