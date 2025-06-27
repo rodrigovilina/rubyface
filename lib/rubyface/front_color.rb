af3eb1ada4ec524002e55e30887e4873fe7a076a
Rodrigo Vilina <roanvilina@gmail.com>
2025-06-26 21:57:58 -0400

    Add `Rubyface::FrontColor`

---
+++ lib/rubyface/front_color.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Rubyface
+  # Color for the front of the card.
+  class FrontColor < T::Enum
+    enums do
+      BLACK = new
+      RED = new
+    end
+  end
+end
---
+++ spec/rubyface/front_color_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubyface::FrontColor do
+  describe 'values' do
+    # rubocop:disable RSpec/Be
+    it 'has five values', :aggregate_failures do
+      expect(described_class::BLACK).to be
+      expect(described_class::RED).to be
+    end
+    # rubocop:enable RSpec/Be
+  end
+end
