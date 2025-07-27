2d86b769ac30c20d63c281d4eb23d5edc62f423f
Rodrigo Vilina <roanvilina@gmail.com>
2025-07-27 16:31:49 -0400

    Update `suit`

--- lib/rubyface/suit.rb
+++ lib/rubyface/suit.rb
@@ -4,12 +4,19 @@
 module Rubyface
   # The `Suit` of a card.
   class Suit < T::Enum
+    extend T::Sig
+
     enums do
       CLUBS = new
       DIAMONDS = new
       HEARTS = new
       JOKERS = new
       SPADES = new
+    end
+
+    sig { returns(T::Array[Suit]) }
+    def self.regular
+      values.reject { |suit| suit == JOKERS }
     end
   end
 end
