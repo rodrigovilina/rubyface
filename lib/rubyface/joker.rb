b1247cc3ceb687010957d5d21d7ce2ccc675907f
Rodrigo Vilina <roanvilina@gmail.com>
2025-07-27 16:31:30 -0400

    Update `Joker`

--- lib/rubyface/joker.rb
+++ lib/rubyface/joker.rb
@@ -10,6 +10,15 @@ module Rubyface
     const :back_color, BackColor
     const :front_color, FrontColor
 
+    def self.all
+      [
+        new(back_color: BackColor::BLUE, front_color: FrontColor::BLACK),
+        new(back_color: BackColor::BLUE, front_color: FrontColor::RED),
+        new(back_color: BackColor::RED, front_color: FrontColor::BLACK),
+        new(back_color: BackColor::RED, front_color: FrontColor::RED),
+      ]
+    end
+
     sig { override.returns(Rank) }
     def rank
       Rank::JOKER
